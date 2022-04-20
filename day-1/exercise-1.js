7adb55fb9000ed4dcf469280ec1e294baa8b8589
Minaro <paturetromain@gmail.com>
2022-04-20 12:09:19 +0200

    add exercise-1.js

---
+++ day-1/exercise-1.js
@@ -0,0 +1,38 @@
+/**
+ * retourne toutes les lettres de l’alphabet en minuscule sur une seule ligne, 
+ * dans l’ordre croissant à partir de la lettre ’a’ jusqu'à 'z'.
+ */
+export const my_display_alpha = () => { 
+    let res = ''
+    const alphabet = [
+        'a',
+        'b',
+        'c',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z',
+    ]
+    for (const letter of alphabet) {
+        res += letter
+    }
+    return res
+}
