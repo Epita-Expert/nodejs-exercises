e8c3eed9e531ce10848770a38658c04003e549dc
Minaro <paturetromain@gmail.com>
2022-04-20 14:38:45 +0200

    Add more test

--- test/exercise-5.test.js
+++ test/exercise-5.test.js
@@ -4,6 +4,9 @@ import assert from "chai";
 describe("exercise 5", function () {
   describe("my_array_alpha", function () {
     it("should return the arg as an array", function () {
+      assert.expect(my_array_alpha("hello"), ["h", "e", "l", "l", "o"]);
+    });
+    it("should return the arg as an array", function () {
       assert.expect(my_array_alpha("hello").length, 5);
     });
   });
